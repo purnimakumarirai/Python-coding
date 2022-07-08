2a24fe85328d826be01425895028d9da6d60eae0
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2022-07-08 17:12:01 +0530

    Add files via upload

---
+++ pythonday1.py
@@ -0,0 +1,7 @@
+#1st program
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("prin('what to print')")
