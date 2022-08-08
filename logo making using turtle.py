88bd9c11979b88eb009052bd4e81f16e4eb1bcfb
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2022-08-08 12:36:51 +0530

    Create logo making using turtle.py

---
+++ logo making using turtle.py
@@ -0,0 +1,17 @@
+from turtle import *
+speed(0)
+bgcolor('lightblue')
+color('hotpink')
+hideturtle()
+n=1
+p=True
+while True:
+           circle(n)
+           if p:
+            n=n-1
+           else:
+            n=n+1
+           if n==0 or n==60:
+            p=not p
+           left(1)
+           forward (3)
