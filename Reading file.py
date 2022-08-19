637d13b20f76f2e824165ef79c3615a14883aa22
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2022-08-19 18:01:03 +0530

    Update Reading file.py

--- Reading file.py
+++ Reading file.py
@@ -2,3 +2,15 @@ file = open("my_file.txt")
 contents = file.read()
 print(contents)
 file.close()
+
+
+
+#my_file.txt
+
+Hello, my name is Purnima.
+
+Specialized with java, python, AWS, HTML, CSS, databases, etc
+
+I have worked on python and HTML projects
+
+Currently, I am working as a product developer
