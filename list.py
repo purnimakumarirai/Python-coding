f0bad3c7a4017e50bb3468d769846522f9a0fc1e
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2022-09-01 10:16:44 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,27 @@
+#1.List into string
+
+apple=['iphone11','iphone12','mac']
+listasstring= ''.(apple)
+print(listasstring)
+
+#2.List into tuple
+
+apple=['iphone11','iphone12','mac']
+listastuple= tuple(apple)
+print(listastuple)
+
+#3.list into a set
+
+apple=['iphone11','iphone12','mac']
+listasset= set(apple)
+print(listasset)
+
+#4.counting occurance
+
+apple=['iphone11','iphone12','mac']
+print(apple.count('iphone11'))
+
+#5.last element in list 
+
+apple=['iphone11','iphone12','mac']
+print(apple[-1][-1])
